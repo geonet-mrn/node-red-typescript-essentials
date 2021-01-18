07549f8e20da363304bbb537a8739d8ca9de439d
Sebastian Bechtold <mr.bechtold@gmail.com>
2021-01-18 13:55:40 +0100

    Added "export" to sleep() function in src/util.ts

--- src/util.ts
+++ src/util.ts
@@ -51,7 +51,7 @@ export function formatNumberString(value: number, numDecimals: number, decimalSe
 
 
 // To be used with "await":
-function sleep(time_ms : number) {
+export function sleep(time_ms : number) {
     return new Promise((resolve) => {
         setTimeout(resolve, time_ms);
     });
